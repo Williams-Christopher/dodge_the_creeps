70e42c2d0a98593c8fe0ecbd800f3ec7563d316f
Christopher Willliams <cw@chriswillia.ms>
2020-01-01 11:43:48 -0600

    Swaps keyboard movement for mouse/touch

--- Player.gd
+++ Player.gd
@@ -4,25 +4,39 @@ signal hit
 
 export var speed = 400 # Player speed pixels/sec
 var screen_size # Size of the game window
+var velocity = Vector2() # Players movement vector
+var target = Vector2() # holds clicked position
 
 func _ready():
 	hide()
 	screen_size = get_viewport_rect().size
 
+func start(pos):
+	position = pos
+	target = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+
 func _process(delta):
-	# Players movement vector
-	var velocity = Vector2()
+#	# Check for input and update the velocity vecotr
+#	if Input.is_action_pressed("ui_right"):
+#		velocity.x += 1
+#	if Input.is_action_pressed("ui_left"):
+#		velocity.x =- 1
+#	if Input.is_action_pressed("ui_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("ui_up"):
+#		velocity.y -= 1
 	
-	# Check for input and update the velocity vecotr
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x =- 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	
+	if position.distance_to(target) > 10:
+		velocity = (target - position).normalized() * speed
+	else:
+		velocity = Vector2()
+
 	# If the player moved, normalize
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -32,8 +46,8 @@ func _process(delta):
 		
 	# Move the player on the screen
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+#	position.x = clamp(position.x, 0, screen_size.x)
+#	position.y = clamp(position.y, 0, screen_size.y)
 	
 	# Play the correct animations
 	if velocity.x != 0:
@@ -44,13 +58,7 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
-
 func _on_Player_body_entered(body):
 	hide()
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
